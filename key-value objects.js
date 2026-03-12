31aa3105230164e8a9852a9e59c39e5efe784b4e
Vaddi Neeraja <neeraja92613@gmail.com>
2026-03-12 14:43:29 +0530

    Add files via upload

---
+++ key-value objects.js
@@ -0,0 +1,10 @@
+let student={
+    name:"Raju",
+    age:20,
+    course:"AI&DS",
+    marks:85
+}
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+console.log(student.marks);
